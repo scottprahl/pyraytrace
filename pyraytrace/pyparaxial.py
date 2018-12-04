d43d999e301dece3ffce5ce23a183ac03dadef55
Scott Prahl <scott@prahls.com>
2018-12-03 22:07:43 -0800

    add ray tracing

--- pyraytrace/pyparaxial.py
+++ pyraytrace/pyparaxial.py
@@ -1,4 +1,5 @@
 # pylint: disable=invalid-name
+# pylint: disable=too-few-public-methods
 """
 Basic routines for ray tracing
 
@@ -28,21 +29,29 @@ class Ray:
     A 2D ray specified by a starting point and an angle
     """
 
-    def __init__(self, z=0, y=0, nu=1):
+    def __init__(self, y=0, nu=1):
         """
         Args:
-            z: axial position of start of ray
             y: distance off axis for start of ray
             nu: angle from axial direction (radians)
         """
-        self.z = z
-        self.ynu = np.array([y,nu])
+        self.ynu = np.array([y, nu], dtype=float)
 
     def __str__(self):
-        return "Ray z=%.3f y=%.3f nu=%.3f" % (self.z, self.ynu[0], self.ynu[1])
+        return "Ray y=%.3f nu=%.3f" % (self.ynu[0], self.ynu[1])
 
     def __repr__(self):
-         return "Ray(z=" + self.z + ", y=" + self.ynu[0] + ", nu=" + self.ynu[1] + ")"
+        return "Ray(y=%f, nu=%f)" % (self.ynu[0], self.ynu[1])
+
+    def aim(self, z0=0, y0=0, z1=1, y1=1):
+        """ aim ray from (z0,y0) to (z1,q1) """
+        self.ynu[0] = y0
+        self.ynu[1] = (y1 - y0) / (z1 - z0)
+
+    def aim2(self, p, q):
+        """ aim ray from point p to point q """
+        self.ynu[0] = p[1]
+        self.ynu[1] = (q[1] - p[1]) / (q[0] - p[0])
 
 
 class ParaxialElement:
@@ -50,24 +59,25 @@ class ParaxialElement:
     A 2D optical element
     """
 
-    def __init__(self, z=0, h=1):
+    def __init__(self, z=0, h=1, color='black'):
         """
         Args:
             z: axial position
         """
         self.z = z
         self.h = h
-        self.color = 'black'
-        self.ABCD = np.array([[1,0], [0,1]])
+        self.ABCD = np.array([[1, 0], [0, 1]], dtype=float)
+        self.color = color
 
     def __str__(self):
         return "ParaxialElement z=%.3f, h=%.3f" % (self.z, self.h)
 
     def __repr__(self):
-         return "ParaxialElement(z=" + self.z + ", h=" + self.h + ")"
-         
+        return "ParaxialElement(z=%f, h=%f)" % (self.z, self.h)
+
     def draw(self):
-        plt.plot([self.z, self.z],[-self.h, self.h],color=self.color)
+        """ Draw the element """
+        plt.plot([self.z, self.z], [-self.h, self.h], color=self.color)
 
 
 class Mirror(ParaxialElement):
@@ -78,51 +88,63 @@ class Mirror(ParaxialElement):
     def __init__(self, z=0, h=1, R=np.inf, color='purple'):
         """
         Args:
-            z: axial position of start of ray
+            z: axial position of mirror
             R: radius of curvature of mirror
             h: height of mirror (half the diameter)
         """
-        self.z = z
+        super().__init__(z=z, h=h, color=color)
         self.R = R
-        self.h = h
-        self.color = color
-        self.ABCD = np.array([[1,0], [2/R, 1]])
+        self.ABCD = np.array([[1, 0], [2/R, 1]], dtype=float)
 
     def __str__(self):
         return "Mirror z=%.3f h=%.3f R=%.3f" % (self.z, self.h, self.R)
 
     def __repr__(self):
-        return "Mirror(z=" + self.z + ", h=" + self.h + ", R=" + self.R + ")"
+        return "Mirror(z=%f, h=%f, R=%f)" % (self.z, self.h, self.R)
 
     def draw(self):
-        plt.plot([self.z, self.z],[-self.h, self.h],color=self.color)
+        """ Draw the mirror """
+        plt.plot([self.z, self.z], [-self.h, self.h], color=self.color)
+
 
 class ThinLens(ParaxialElement):
     """
     A thin lens element
     """
 
-    def __init__(self, z=0, h=1, f=10, color='blue'):
+    def __init__(self, z=0, h=1, f=10, width=0.2, color='blue'):
         """
         Args:
-            z: axial position of start of ray
+            z: axial position of thin lens
             f: focal length of mirror
             h: height of lens (half the diameter)
+            width: width of arrowhead
         """
-        self.z = z
+        super().__init__(z=z, h=h, color=color)
         self.f = f
-        self.h = h
-        self.color = color
-        self.ABCD = np.array([[1,0], [-1/f, 1]])
+        self.width = width
+        self.ABCD = np.array([[1, 0], [-1/f, 1]], dtype=float)
 
     def __str__(self):
         return "ThinLens z=%.3f h=%.3f f=%.3f" % (self.z, self.h, self.f)
 
     def __repr__(self):
-         return "ThinLens(z=" + self.z + ", h=" + self.h + ", f=" + self.f + ")"
+        return "ThinLens(z=%f, h=%f, f=%f)" % (self.z, self.h, self.f)
 
     def draw(self):
-        plt.plot([self.z, self.z],[-self.h, self.h],color=self.color)
+        """ Draw the thin lens """
+        w = self.h * self.width
+        plt.plot([self.z, self.z], [-self.h, self.h], color=self.color)
+        if self.f > 0:
+            plt.plot([self.z-w, self.z, self.z+w],
+                     [self.h-w, self.h, self.h-w], color=self.color)
+            plt.plot([self.z-w, self.z, self.z+w],
+                     [-self.h+w, -self.h, -self.h+w], color=self.color)
+        else:
+            plt.plot([self.z-w, self.z, self.z+w],
+                     [self.h+w, self.h, self.h+w], color=self.color)
+            plt.plot([self.z-w, self.z, self.z+w],
+                     [-self.h-w, -self.h, -self.h-w], color=self.color)
 
 
 class Plane(ParaxialElement):
@@ -130,25 +152,23 @@ class Plane(ParaxialElement):
     A plane element
     """
 
-    def __init__(self, z=0, h=1):
+    def __init__(self, z=0, h=1, color='black'):
         """
         Args:
-            z: axial position of start of ray
+            z: axial position of plane
             h: lateral extent of plane above optical axis
         """
-        self.z = z
-        self.h = h
-        self.color = 'orange'
-        self.ABCD = np.array([[1,0], [0,1]])
+        super().__init__(z=z, h=h, color=color)
 
     def __str__(self):
         return "Plane z=%.3f h=%.3f" % (self.z, self.h)
 
     def __repr__(self):
-         return "Plane(z=" + self.z + ", h=" + self.h + ")"
+        return "Plane(z=%f, h=%f)" % (self.z, self.h)
 
     def draw(self):
-        plt.plot([self.z, self.z],[-self.h, self.h],color=self.color)
+        """ Draw the plane """
+        plt.plot([self.z, self.z], [-self.h, self.h], ':', color=self.color)
 
 
 class Aperture(Plane):
@@ -156,34 +176,31 @@ class Aperture(Plane):
     An aperture element
     """
 
-    def __init__(self, z=0, h=1, D=1, width=0.3):
+    def __init__(self, z=0, h=1, D=1, width=0.3, color='black'):
         """
         Args:
-            z: axial position of start of ray
+            z: axial position of aperture
             D: diameter of aperture
-            h: lateral extent of aperture
+            h: overall height of aperture (needed for drawing)
             width: fraction of aperture for horizontal bars
         """
-        self.z = z
+        super().__init__(z=z, h=h, color=color)
         self.D = D
-        self.h = h
         self.width = width
-        self.color = 'black'
-        self.ABCD = np.array([[1,0], [0,1]])
 
     def __str__(self):
         return "Aperture z=%.3f h=%.3f D=%.3f width=%.3f" % (self.z, self.h, self.D, self.width)
 
     def __repr__(self):
-         return "Aperture(z=" + self.z + ", h=" + self.h + ", D=" + self.D + ")"
+        return "Aperture(z=%f, h=%f, D=%f)" % (self.z, self.h, self.D)
 
     def draw(self):
         r = self.D/2
-        w = self.width * r / 2
-        plt.plot([self.z,  self.z  ],[ r, self.h],color=self.color)
-        plt.plot([self.z-w,self.z+w],[ r,r],color=self.color)
-        plt.plot([self.z,  self.z  ],[-r,-self.h],color=self.color)
-        plt.plot([self.z-w,self.z+w],[-r,-r],color=self.color)
+        w = self.width * self.h / 2
+        plt.plot([self.z, self.z], [r, self.h], color=self.color)
+        plt.plot([self.z-w, self.z+w], [r, r], color=self.color)
+        plt.plot([self.z, self.z], [-r, -self.h], color=self.color)
+        plt.plot([self.z-w, self.z+w], [-r, -r], color=self.color)
 
 
 class Object(Plane):
@@ -191,26 +208,25 @@ class Object(Plane):
     An object with height h
     """
 
-    def __init__(self, z=0, h=1):
+    def __init__(self, z=0, h=1, color='red'):
         """
         Args:
             z: axial position of start of ray
             h: lateral extent of aperture
         """
-        self.z = z
-        self.h = h
-        self.color = 'red'
-        self.ABCD = np.array([[1,0], [0,1]])
+        super().__init__(z=z, h=h, color=color)
 
     def __str__(self):
         return "Object z=%.3f h=%.3f" % (self.z, self.h)
 
     def __repr__(self):
-         return "Object(z=" + self.z + ", h=" + self.h + ")"
+        return "Object(z=%f, h=%f)" % (self.z, self.h)
 
     def draw(self):
-        plt.plot([self.z, self.z],[0,self.h],color=self.color)
-        
+        #        plt.plot([self.z, self.z], [0, self.h], color=self.color)
+        plt.annotate("", xy=(self.z, 0), xytext=(self.z, self.h),
+                     arrowprops=dict(arrowstyle="<-"))
+
 
 class ParaxialSystem:
     """
@@ -219,29 +235,55 @@ class ParaxialSystem:
 
     def __init__(self):
         self.elements = []
-        
+
     def __str__(self):
-        str = ''
+        s = ''
         for e in self.elements:
-            str = str + e.__str__() + "\n"
-        return str
+            s += e.__str__() + "\n"
+        return s
 
     def __repr__(self):
-        repr = ''
+        r = ''
         for e in self.elements:
-            repr = repr + e.__repr__() + "\n"
-        return repr
+            r += e.__repr__() + "\n"
+        return r
 
     def append(self, elem):
+        """ add another optical element to the system """
         self.elements.append(elem)
-        
-    def move(self, ray):
-        self.draw()
-            
-        for i,e in enumerate(self.elements):
-            print(i,e)
-            
+
+    def minmax(self):
+        """ return min and max z """
+        zmin = 1e6
+        zmax = -1e6
+        for e in self.elements:
+            if e.z < zmin:
+                zmin = e.z
+            if zmax < e.z:
+                zmax = e.z
+        return zmin, zmax
+
+    def draw_ray_through_system(self, ray, color='black'):
+        """ Draw the ray as it moves through the system """
+
+        z_last = None
+        ynu = np.copy(ray.ynu)
+
+        for e in self.elements:
+            if z_last != None:
+                prop = np.array([[1, e.z-z_last], [0, 1]])
+
+                ynu_next = np.matmul(prop, ynu)
+                plt.plot([z_last, e.z], [ynu[0], ynu_next[0]], color=color)
+
+                ynu_bent = np.matmul(e.ABCD, ynu_next)
+                ynu = np.copy(ynu_bent)
+            z_last = e.z
+
     def draw(self):
-        plt.plot([self.elements[0].z,self.elements[-1].z],[0,0],'k:')
+        """ Draw all the elements """
+        zmin, zmax = self.minmax()
+
+        plt.plot([zmin, zmax], [0, 0], 'k')
         for e in self.elements:
             e.draw()
